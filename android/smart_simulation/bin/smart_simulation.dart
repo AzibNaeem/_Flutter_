398b38ee35cc769cb6e3b4b0a4e7d89a9ca9c1db
arinaashraf <arinaashraf20@gmail.com>
2025-07-15 10:57:34 +0500

    test changes to trigger PR

---
+++ android/smart_simulation/bin/smart_simulation.dart
@@ -0,0 +1,5 @@
+import 'package:smart_simulation/smart_simulation.dart' as smart_simulation;
+
+void main(List<String> arguments) {
+  print('Hello world: ${smart_simulation.calculate()}!');
+}
---
+++ android/smart_simulation/test/smart_simulation_test.dart
@@ -0,0 +1,8 @@
+import 'package:smart_simulation/smart_simulation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
