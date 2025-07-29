91182309bae2b7b0edb53025472864528dfacf52
miznar <2021cs174@student.uet.edu.pk>
2025-07-29 15:52:22 +1200

    Update Branch

--- lib/core/API/api_constants.dart
+++ lib/core/API/api_constants.dart
@@ -1,9 +1,7 @@
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.1.190:8000';
-  static const String baseUrl2 = 'http://192.1.190.16:8000';
+  static const String baseUrl = 'http://192.1.190.16:8000';
   static const String loginEndpoint = '/users/validate';
   static const String baseUrl1 = 'https://6875fe28814c0dfa653a2485.mockapi.io';
   static const String leaveRequestsEndpoint = '$baseUrl1/leaveRequests';
-  static const String travelRequestEndpoint='/travel/submit';
 }
 
